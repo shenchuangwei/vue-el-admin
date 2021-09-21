e335edaa278cf0e6351611f28dee6a434b3d46c4
MrShen <2623147902@qq.com>
2021-09-21 14:11:32 +0800

    相册组件模块已分离

---
+++ src/components/image/album-item.vue
@@ -0,0 +1,30 @@
+<template>
+  <li class="list-group-item list-group-item-action d-flex align-items-center"
+      @click.stop="$emit('albumChange',index)" style="line-height: 24px;cursor: pointer"
+      :class="{'active sum-active':albumIndex === index}">{{ item.name }}
+    <el-dropdown class="btn ml-auto">
+                <span class="btn btn-light btn-sm border" size="mini" style="font-size: 12px">
+                  {{ item.num }}<i class="el-icon-arrow-down el-icon--right"></i>
+                </span>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item @click.stop.native="$emit('albumDel',index)">删除</el-dropdown-item>
+        <el-dropdown-item @click.stop.native="$emit('openEditModel',{item,index})">修改</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "album-item",
+  props: {
+    item: Object,
+    index: Number,
+    albumIndex: Number
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -12,26 +12,16 @@
                     style="width: 150px;"></el-input>
           <el-button type="success" size="mini">搜索</el-button>
         </div>
-        <el-button type="success" size="mini">创建相册</el-button>
-        <el-button type="warning" size="mini">上传图片</el-button>
+        <el-button type="success" size="mini" @click="openEditModel(false)">创建相册</el-button>
+        <el-button type="warning" size="mini" @click="albumUploadModel = true">上传图片</el-button>
       </el-header>
       <el-container>
         <el-aside width="200px">
           <!--侧边列表组-->
           <ul class="list-group list-group-flush" style="font-size: 12px">
-            <li v-for="(item,index) of albums" class="list-group-item list-group-item-action d-flex align-items-center"
-                :key="index" @click.stop="albumChange(index)" style="line-height: 24px;cursor: pointer"
-                :class="{'active sum-active':albumIndex === index}">{{ item.name }}
-              <el-dropdown class="btn ml-auto">
-                <span class="btn btn-light btn-sm border" size="mini" style="font-size: 12px">
-                  {{ item.num }}<i class="el-icon-arrow-down el-icon--right"></i>
-                </span>
-                <el-dropdown-menu slot="dropdown">
-                  <el-dropdown-item>黄金糕</el-dropdown-item>
-                  <el-dropdown-item>狮子头</el-dropdown-item>
-                </el-dropdown-menu>
-              </el-dropdown>
-            </li>
+
+            <albumItem v-for="(item,index) of albums" :key="index" :item="item" :index="index" :albumIndex='albumIndex' @albumChange="albumChange" @albumDel="albumDel" @openEditModel="openEditModel"/>
+
           </ul>
         </el-aside>
         <el-container>
@@ -44,12 +34,44 @@
         <!--底部-->
       </el-footer>
     </el-container>
+
+    <!--修改弹出层-->
+    <el-dialog :title="modelTitle" :visible.sync="albumEditModel" width="30%">
+      <!--表单项-->
+      <el-form ref="form" :model="albumForm" label-width="80px">
+        <el-form-item label="相册名称">
+          <el-input v-model="albumForm.name"></el-input>
+        </el-form-item>
+        <el-form-item label="相册排序">
+          <el-input-number v-model="albumForm.order" :min="0"></el-input-number>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="albumEditModel = false">取 消</el-button>
+        <el-button type="primary" @click="confirmAlbumModel">确 定</el-button>
+      </span>
+    </el-dialog>
+    <!--修改弹出层-->
+    <el-dialog title="上传图片" :visible.sync="albumUploadModel">
+      <el-upload
+          class="upload-demo text-center"
+          drag
+          action="https://jsonplaceholder.typicode.com/posts/"
+          multiple>
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+        <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+      </el-upload>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+import albumItem from '@/components/image/album-item'
+
 export default {
   name: "index",
+  components: { albumItem },
   data() {
     return {
       searchForm: {
@@ -58,7 +80,22 @@ export default {
       },
       //当前相册选中项
       albumIndex: 0,
-      albums: []
+      albums: [],
+      //修改,上传弹出层是否可见
+      albumUploadModel: false,
+      albumEditModel: false,
+      albumEditIndex: -1,
+      //相册修改表单对象
+      albumForm: {
+        name: '',
+        order: 0
+      }
+    }
+  },
+  computed:{
+    modelTitle() {
+      //判断弹出层标题title
+      return this.albumEditIndex > -1 ? '修改相册' : '添加相册'
     }
   },
   created() {
@@ -76,6 +113,54 @@ export default {
     //检测左侧相册列表变化事件
     albumChange(index) {
       this.albumIndex = index
+    },
+    //打开删除相册弹出层，删除相册
+    albumDel(index) {
+      this.$confirm('此操作将永久删除该相册, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.albums.splice(index, 1)
+        this.$message.success('删除成功')
+      }).catch(() => {
+        this.$message.info('已取消删除')
+      });
+    },
+    //打开修改弹出层
+    openEditModel(albumObj) {
+      //修改相册
+      if (albumObj) {
+        const {item, index} = albumObj
+        this.albumForm.name = item.name
+        this.albumForm.order = item.order
+        this.albumEditIndex = index
+        return this.albumEditModel = true
+      }
+      //添加相册
+      this.albumForm.name = ''
+      this.albumForm.order = 0
+      this.albumEditIndex = -1
+      this.albumEditModel = true
+    },
+    //点击确定修改/创建相册
+    confirmAlbumModel() {
+      if (this.albumEditIndex > -1) {
+        this.albumEdit();
+        this.$message.success('修改相册成功')
+        return this.albumEditModel = false
+      }
+      //创建相册,向相册数组前面添加新的一项
+      this.albums.unshift(
+          {name: this.albumForm.name, order: this.albumForm.order, num: 0}
+      )
+      this.$message.success('修改添加成功')
+      this.albumEditModel = false
+    },
+    albumEdit() {
+      //修改对应索引相册
+      this.albums[this.albumEditIndex].name = this.albumForm.name
+      this.albums[this.albumEditIndex].order = this.albumForm.order
     }
   }
 }
